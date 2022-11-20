368b07e32e0e59e2a462bb939373ee0b1519e88b
lschulzes <lucassilvaiee@gmail.com>
2022-11-20 19:10:24 -0300

    initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import "@testing-library/jest-dom";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Vite \+ React⚡⚡/i);
+  expect(linkElement).toBeInTheDocument();
+});
